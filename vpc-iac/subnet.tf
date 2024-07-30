0e554f27e63bb172bada8f8a3e77284401019c32
SachsAlex <alexander.sachs@docc.techstarter.de>
2024-07-30 09:32:08 +0200

    update_subnet.tf azs

--- vpc-iac/subnet.tf
+++ vpc-iac/subnet.tf
@@ -1,19 +1,25 @@
 resource "aws_subnet" "public_1" {
-  vpc_id     = aws_vpc.my_vpc.id
-  cidr_block = "10.0.1.0/24"
+  vpc_id                  = aws_vpc.my_vpc.id
+  cidr_block              = "10.0.1.0/24"
+  availability_zone       = "eu-central-1a"
+  map_public_ip_on_launch = true
 }
 
 resource "aws_subnet" "private_1" {
-  vpc_id     = aws_vpc.my_vpc.id
-  cidr_block = "10.0.2.0/24"
+  vpc_id            = aws_vpc.my_vpc.id
+  cidr_block        = "10.0.2.0/24"
+  availability_zone = "eu-central-1a"
 }
 
 resource "aws_subnet" "public_2" {
-  vpc_id     = aws_vpc.my_vpc.id
-  cidr_block = "10.0.3.0/24"
+  vpc_id                  = aws_vpc.my_vpc.id
+  cidr_block              = "10.0.3.0/24"
+  availability_zone       = "eu-central-1b"
+  map_public_ip_on_launch = true
 }
 
 resource "aws_subnet" "private_2" {
-  vpc_id     = aws_vpc.my_vpc.id
-  cidr_block = "10.0.4.0/24"
+  vpc_id            = aws_vpc.my_vpc.id
+  cidr_block        = "10.0.4.0/24"
+  availability_zone = "eu-central-1b"
 }
